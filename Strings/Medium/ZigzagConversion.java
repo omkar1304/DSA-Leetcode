d4f25df3536ee5f5c1e71f0089ff712752588a11
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2023-04-20 21:10:34 +0530

    "Happy Coding"

--- Strings/Medium/ZigzagConversion.java
+++ Strings/Medium/ZigzagConversion.java
@@ -20,3 +20,33 @@ public class ZigzagConversion {
         return res;
     }
 }
+
+// optimized ->
+class Solution {
+    public String convert(String s, int numRows) {
+        
+        char[] c = s.toCharArray();
+        int len = c.length;
+        
+        StringBuilder[] sb = new StringBuilder[numRows];
+        
+        for(int i=0; i<sb.length; i++)
+            sb[i] = new StringBuilder();
+        
+        int index = 0;
+        
+        while(index < len){
+            // vertically down
+            for(int idx=0; idx<numRows && index<len; idx++)
+                sb[idx].append(c[index++]);
+            // obliquely up
+            for(int idx=numRows-2; idx>0 && index<len; idx--)
+                sb[idx].append(c[index++]);
+        }
+        
+        for(int i=1; i<sb.length; i++)
+            sb[0].append(sb[i]);
+        
+        return sb[0].toString();
+    }
+}

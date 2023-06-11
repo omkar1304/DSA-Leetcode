e773fab646cde973dcebf0a53bdb2abaa43f532d
Omkar Pedamkar <omkarpedamkar2@gmail.com>
2023-06-11 13:42:21 +0530

    "Happy Coding"

---
+++ Arrays/Easy/CheckIfTheNumberIsFascinating.java
@@ -0,0 +1,41 @@
+// https://leetcode.com/problems/check-if-the-number-is-fascinating/
+
+class Solution {
+    public boolean isFascinating(int n) {
+        
+        // check every no seperately and mark there element in visited array. if any element is already visited then or its 0 then return false else return true
+        
+        boolean[] visited = new boolean[10];
+        
+        if(!check(n, visited))
+            return false;
+        
+        if(!check(2*n, visited))
+            return false;
+        
+        if(!check(3*n, visited))
+            return false;
+        
+        return true;
+    }
+    
+    public boolean check(int n, boolean[] visited){
+        
+        while(n > 0){
+            
+            int temp = n % 10;
+            
+            if(temp == 0)
+                return false;
+            
+            if(visited[temp] == true)
+                return false;
+            
+            visited[temp] = true;
+            n = n / 10;
+                            
+        }
+        
+        return true;
+    }
+}
---
+++ Strings/Medium/FindLongestSemiRepetitiveSubstring.java
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/find-the-longest-semi-repetitive-substring/
+
+class Solution {
+    public int longestSemiRepetitiveSubstring(String s) {
+        
+        // to store index of last pair of same char
+        int lastPairIndex = -1;
+        // index of start of current string
+        int start = 0;
+        // to store max length 
+        int max = 1;
+        
+        for(int end=1; end<s.length(); end++){
+            
+            // if both char are same then we need to store index of it
+            if(s.charAt(end) == s.charAt(end-1)){
+                
+                // if lastPairIndex is not at -1 then its already found pair so we have start new string so update start 
+                if(lastPairIndex != -1)
+                    start = lastPairIndex;
+                
+                // storing index of last pair
+                lastPairIndex = end;
+            }
+            
+            // everytime store current string length and take max out of it
+            max = Math.max(max, (end - start + 1));
+        }
+        
+        return max;
+    }
+}
